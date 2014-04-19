93d87994c0b86f3f2fa255d2e99b9ba713b5a1c6
electronic Max <electronic@gmail.com>
2014-04-19 17:04:29 +0100

    scribble happy

--- apps/vitality/genkeys.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-openssl genrsa -out privkey.pem 1024
-openssl rsa -pubout -in privkey.pem -out pubkey.pem
-echo "private key >> "
-cat privkey.pem
-echo "public key >>"
-cat pubkey.pem
